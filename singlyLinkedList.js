ceb042e51718ae0b4f390efdd959b04396d4b0b5
mithun.k <mithun.k@borqs.com>
2024-10-06 15:30:32 +0530

    linked list complete

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -44,6 +44,69 @@ class LinkedList {
         this.size++;
     }
 
+    insert(value, index) {
+        if (index < 0 || index > this.size) {
+            return `The index: ${index} is not in the LIST!`;
+        }
+        if (index === 0) {
+            //Call the prepend method to add one in the beginning of the LIST
+            this.prepend(value);
+        }
+        if (index > 0 && index <= this.size) {
+            let node = new Node(value);
+            let prevNode = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                prevNode = prevNode.next
+            }
+            node.next = prevNode.next;
+            prevNode.next = node;
+            this.size++
+        }
+    }
+
+    removeFromIndex(index) {
+        if (index < 0 || index >= this.size) {
+            return null
+        }
+        let removedNode;
+        if (index === 0) {
+            removedNode = this.head;
+            this.head = this.head.next
+        } else {
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            removedNode = prev.next;
+            prev.next = removedNode.next
+        }
+        this.size--
+        return removedNode
+    }
+
+    removeByValue(value) {
+        if (this.isEmpty()) {
+            return null;
+        }
+        if (this.head.value === value) {
+            this.head = this.head.next;
+            this.size--;
+            return value;
+        } else {
+            let prev = this.head
+            while (prev.next && prev.next.value !== value) {
+                prev = prev.next
+            }
+            if (prev.next) {
+                let removedNode = prev.next;
+                prev.next = removedNode.next
+                this.size--;
+                return removedNode;
+            }
+            return null
+        }
+    }
+
     print() {
         if (this.isEmpty()) {
             console.log("List is empty");
@@ -63,13 +126,9 @@ let list = new LinkedList();
 console.log("Is empty: ", list.isEmpty());
 console.log("Size of the list: ", list.getSize());
 list.print()
-list.append(10);
-list.append(20);
-list.append(30);
-console.log("Is empty: ", list.isEmpty());
-console.log("Size of the list: ", list.getSize());
+list.append(10)
+list.append(20)
+list.insert(40, 1)
 list.print()
-list.append(40);
-list.prepend(5);
+console.log(list.removeByValue(60))
 list.print()
-// console.log();
